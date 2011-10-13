be8e061da80b425662c80bd35a0d97a10157cca4
chris.watts.t@gmail.com <chris.watts.t@gmail.com@a66fea9a-887d-1d9c-331d-2dd18522cd1f>
2011-10-13 00:19:39 +0000

    Updated the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
-echo hek
+#! /bin/bash
+echo User : ${USER}
+echo Copying Client Website to Server.
+scp -r ./ClientWebsite/ ${USER}@140.160.138.28:"/ProjectSites/2011/Fall/Tournament\\ Scheduling/"
+
+echo Copying Java Server to Server.
+scp -r ./EndpointProgram/ ${USER}@140.160.138.28:"/ProjectSites/2011/Fall/Tournament\\ Scheduling/"
+
+echo Starting Endpiont Server.
+ssh ${USER}@140.160.138.28 -C "(nohup java -jar /ProjectSites/2011/Fall/Tournament\ Scheduling/EndpointProgram/Endpoint)"
