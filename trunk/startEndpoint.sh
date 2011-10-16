8894c4e8bca83f2d852135777a7c13443a13139f
chris.watts.t@gmail.com <chris.watts.t@gmail.com@a66fea9a-887d-1d9c-331d-2dd18522cd1f>
2011-10-16 20:57:26 +0000

    Added more helper scripts

--- trunk/startEndpoint.sh
+++ trunk/startEndpoint.sh
@@ -2,9 +2,11 @@
 
 projectserver=${USER}@140.160.138.28
 
+# Kill any previous servers that are running
+sh stopEndpoint.sh
+
 echo -===Starting Endpoint Server===-
 
-# Kill any previous servers that are running
 # cd to the Project directory so that when we start the program all the log files will be created in the correct location
 # Start the server
-ssh ${projectserver} -C "(pkill -9 java; cd /ProjectSites/2011/Fall/Tournament\\ Scheduling/; java -jar Endpoint.jar)"
+ssh ${projectserver} -C "(cd /ProjectSites/2011/Fall/Tournament\\ Scheduling/; java -jar Endpoint.jar)"
