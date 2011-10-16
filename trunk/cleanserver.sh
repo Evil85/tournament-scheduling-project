1d8e886f1889570ee996d796dc2684fd21b65c54
chris.watts.t@gmail.com <chris.watts.t@gmail.com@a66fea9a-887d-1d9c-331d-2dd18522cd1f>
2011-10-16 00:24:40 +0000

    Fixed deploy scripts

--- trunk/cleanserver.sh
+++ trunk/cleanserver.sh
@@ -2,8 +2,8 @@
 
 projectserver=${USER}@140.160.138.28
 
-echo -===Cleaning Endpoing Server from Project Server===-
+echo -===Cleaning Project Server===-
 
 #cd to the project server and remove the jars
-ssh ${projectserver} -C "(cd /ProjectSites/2011/Fall/Tournament\\ Scheduling/; nohup rm Endpoint.jar gson-1.7.1.jar log4j-1.2.16.jar)"
+ssh ${projectserver} -C "(cd /ProjectSites/2011/Fall/Tournament\\ Scheduling/; rm -rf Endpoint.jar gson-1.7.1.jar log4j-1.2.16.jar ./ClientWebsite/)"
 
