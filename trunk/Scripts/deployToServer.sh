381a6a8e2caa4c453b8ee643fc94d219c0651909
chris.watts.t@gmail.com <chris.watts.t@gmail.com@a66fea9a-887d-1d9c-331d-2dd18522cd1f>
2011-10-17 01:17:55 +0000

    Added more helper scripts

--- trunk/Scripts/deployToServer.sh
+++ trunk/Scripts/deployToServer.sh
@@ -13,17 +13,13 @@ sh ./copyWebsiteToServer.sh
 echo -===Copying Endpoint Server to Project Server===-
   
 # Copy the Endpoint jar and external libraries to the project server.
-scp ./Endpoint.jar ./gson-1.7.1.jar ./log4j-1.2.16.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
-#scp ./gson-1.7.1.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
-#scp ./log4j-1.2.16.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
+scp ../out/Endpoint.jar ../out/gson-1.7.1.jar ../out/log4j-1.2.16.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
+
 echo -===Setting permisions===-
 ssh ${projectserver} -C "(cd /ProjectSites/2011/Fall/Tournament\\ Scheduling/; chmod 775 ./ClientWebsite/* Endpoint.jar ./gson-1.7.1.jar ./log4j-1.2.16.jar ./Logs/*)"
 
-echo -===Cleaing up deploy temp files===-
-
-rm ./Endpoint.jar
-rm ./gson-1.7.1.jar
-rm ./log4j-1.2.16.jar
+#echo -===Cleaing up deploy temp files===-
+sh cleanEndpoint.sh
 
 # Call the start server script
 sh ./startEndpointOnServer.sh
---
+++ trunk/Scripts/stopEndpointOnLocal.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo -===Stopping Endpoint Server===-
+
+# Kill the server if it was running
+pkill -9 java
+
