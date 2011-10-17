a00554ccbcc47b564207ed6ae4c87b9030d8fc5a
chris.watts.t@gmail.com <chris.watts.t@gmail.com@a66fea9a-887d-1d9c-331d-2dd18522cd1f>
2011-10-17 00:55:07 +0000

    Fixed deploy script to include changing permissions

--- trunk/Scripts/deployToServer.sh
+++ trunk/Scripts/deployToServer.sh
@@ -13,9 +13,11 @@ sh ./copyWebsiteToServer.sh
 echo -===Copying Endpoint Server to Project Server===-
   
 # Copy the Endpoint jar and external libraries to the project server.
-scp ./Endpoint.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
-scp ./gson-1.7.1.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
-scp ./log4j-1.2.16.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
+scp ./Endpoint.jar ./gson-1.7.1.jar ./log4j-1.2.16.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
+#scp ./gson-1.7.1.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
+#scp ./log4j-1.2.16.jar ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling"
+echo -===Setting permisions===-
+ssh ${projectserver} -C "(cd /ProjectSites/2011/Fall/Tournament\\ Scheduling/; chmod 775 ./ClientWebsite/* Endpoint.jar ./gson-1.7.1.jar ./log4j-1.2.16.jar ./Logs/*)"
 
 echo -===Cleaing up deploy temp files===-
 
