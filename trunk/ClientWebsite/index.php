04345d35b23978b1b9969b0af9ebf30b66c95d63
spiderrobb@gmail.com <spiderrobb@gmail.com@a66fea9a-887d-1d9c-331d-2dd18522cd1f>
2011-10-31 03:38:26 +0000

    new index

--- trunk/ClientWebsite/index.php
+++ trunk/ClientWebsite/index.php
@@ -1,16 +1,9 @@
 <?php
-	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-	socket_connect($socket, "localhost", "8000");
-	socket_write($socket, "{ Command : createUser, Username : Bob }\n\r");
-	$result = "";
-	while ($read = socket_read($socket, 2024))
-	{  
-		if ($read == '\r')
-			break;
-		$result .= $read;
-		echo $result;
-	}
-										 
-	echo "Result recieved: '$result'\n";
-	socket_close($socket);
+require_once 'functions.php';
+$data = array(
+	'page' => 'Page_Index',
+	'title'    => 'Home'
+);
+$page = new PageBuilder($data);
+$page->generate();
 ?>
