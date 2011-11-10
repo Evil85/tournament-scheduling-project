bcbace3ccb19bad4948673cd035d2400e91846d9
chris.watts.t@gmail.com <chris.watts.t@gmail.com@a66fea9a-887d-1d9c-331d-2dd18522cd1f>
2011-11-10 20:54:33 +0000

    Fixed the assamble db script

--- trunk/Scripts/assembleDB.sh
+++ trunk/Scripts/assembleDB.sh
@@ -3,5 +3,5 @@
 projectserver=${USER}@140.160.138.28
 
 echo -===Assembling Database===-
-
-ssh ${projectserver} -C "(mysql --user=admtourn201140 --password=yinvamOph -D tourn_201140 < TournDB_Create_Script.sql)"
+scp TournDB_Create_Script.sql ${projectserver}:"/ProjectSites/2011/Fall/Tournament\\ Scheduling/"
+ssh ${projectserver} -C "(cd /ProjectSites/2011/Fall/Tournament\\ Scheduling/; chmod 777 TournDB_Create_Script.sql; mysql --user=admtourn201140 --password=yinvamOph -D tourn_201140 < TournDB_Create_Script.sql)"
