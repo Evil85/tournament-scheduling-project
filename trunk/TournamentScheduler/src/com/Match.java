8fb204ccf7ba686eba251f00c5ba5d5bf346a61b
pleatherwill@gmail.com <pleatherwill@gmail.com@a66fea9a-887d-1d9c-331d-2dd18522cd1f>
2011-12-01 16:37:55 +0000

    Moving a method that really shouldn't have been where it was.

--- trunk/TournamentScheduler/src/com/Match.java
+++ trunk/TournamentScheduler/src/com/Match.java
@@ -1,8 +1,12 @@
 package com;
 
+import java.sql.Timestamp;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.Vector;
+import com.SchedulingUtil.TruncateSide;
 
 public class Match {
 
@@ -17,6 +21,72 @@ public class Match {
 		m_court = null;
 		m_time = null;
 		m_parents = new Vector<Match>();
+	}
+	
+	public SortedSet<TimeSpan> Availability(Court c, Vector<Match> previouslyScheduled, AvailabilityType type)
+	{
+		Vector<TimeConstraint> constraints = new Vector<TimeConstraint>(Players());
+		constraints.add(c);
+		SortedSet<TimeSpan> availableTimes = SchedulingUtil.IntersectAvailability(constraints.toArray(new TimeConstraint[0]));
+		SortedSet<TimeSpan> discomfortTimes = new TreeSet<TimeSpan>();
+
+		// Truncate the available times for any Matches for which this Match is based on the outcome.
+		for (Match parent : Parents())
+			if (parent.Time() != null)
+				SchedulingUtil.Truncate(availableTimes, TruncateSide.Start, parent.Time().getEnd());
+
+		for(Match other : previouslyScheduled)
+		{
+			TimeSpan otherTime = other.Time();
+			if (otherTime != null)
+			{
+				// If the other match is dependent on the outcome of this match, truncate the available times.
+				if (other.Parents().contains(this))
+					if (other.Time() != null)
+						SchedulingUtil.Truncate(availableTimes, TruncateSide.End, other.Time().getStart());
+
+				// If the other match shares players with this one, exclude the appropriate time window.
+				if (SharesPlayers(other))
+				{
+					if (c.Venue() != other.Court().Venue())
+					{
+						Timestamp travelTimeStart = new Timestamp(otherTime.getStart().getTime() - c_nMsTravelTime);
+						Timestamp travelTimeEnd = new Timestamp(otherTime.getEnd().getTime() + c_nMsTravelTime);
+						TimeSpan travelTimeWindow = new TimeSpan(travelTimeStart, travelTimeEnd);
+						SchedulingUtil.RemoveAvailability(availableTimes, travelTimeWindow);
+					}
+					else
+					{
+						SchedulingUtil.RemoveAvailability(availableTimes, otherTime);
+					}
+
+					if (type != AvailabilityType.All)
+					{
+						Timestamp discomfortStart = new Timestamp(otherTime.getStart().getTime() - c_nMsComfortWindow);
+						Timestamp discomfortEnd = new Timestamp(otherTime.getEnd().getTime() + c_nMsComfortWindow);
+						SchedulingUtil.AddAvailability(discomfortTimes, new TimeSpan(discomfortStart, discomfortEnd));
+					}
+				}
+				else if (c == other.Court())
+				{
+					SchedulingUtil.RemoveAvailability(availableTimes, otherTime);
+				}
+			}
+		}
+
+		switch (type)
+		{
+			case All:
+				return availableTimes;
+			case Comfort:
+				for (TimeSpan span : discomfortTimes)
+					SchedulingUtil.RemoveAvailability(availableTimes, span);
+				return availableTimes;
+			case Discomfort:
+				return SchedulingUtil.IntersectAvailability(availableTimes, discomfortTimes);
+			default:
+				throw new UnsupportedOperationException();
+		}
 	}
 
 	public void AddParent(Match parent)
@@ -92,5 +162,11 @@ public class Match {
 	private Set<Player> m_players;
 	private TimeSpan m_time;
 	private Vector<Match> m_parents;
+	
+	// 3,600,000 ms = 1 hour;
+	private static final int c_nMsTravelTime = 3600000;
+
+	// 1,800,000 ms = 30 minutes;
+	private static final int c_nMsComfortWindow = 1800000;
 
 }
