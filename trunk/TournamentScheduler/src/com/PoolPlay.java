862bebb1dc43a4bf2de2bde5eb447ae3121a6c7d
pleatherwill@gmail.com <pleatherwill@gmail.com@a66fea9a-887d-1d9c-331d-2dd18522cd1f>
2011-12-06 23:08:38 +0000

    adding match numbers

--- trunk/TournamentScheduler/src/com/PoolPlay.java
+++ trunk/TournamentScheduler/src/com/PoolPlay.java
@@ -42,19 +42,25 @@ public class PoolPlay extends Division {
 		Player[] firstRoundPlayers = firstRound.toArray(new Player[0]);
 		Player[] secondRoundPlayers = secondRound.toArray(new Player[0]);
 
+		int nTeams1 = teams.length / 2;
+		int nTeams2 = teams.length - nTeams1;
+		int nMatchId = (nTeams1 * (nTeams1 - 1)) / 2 +
+			(nTeams2 * (nTeams2 - 1)) / 2 +
+			3;
+
 		Team finalistA =  new Team(-1, "Finalist A", firstRoundPlayers);
 		Team finalistB =  new Team(-2, "Finalist B", secondRoundPlayers);
-		Match finalMatch = new Match(String.format("%s final match", m_strName), this, finalistA, finalistB);
+		Match finalMatch = new Match(--nMatchId, String.format("%s final match", m_strName), this, finalistA, finalistB);
 		m_unscheduled.add(finalMatch);
 
 		Team winnerRoundA = new Team(-3, "Winner A", firstRoundPlayers);
 		Team runnerRoundA = new Team(-4, "Runner-up A", firstRoundPlayers);
 		Team winnerRoundB = new Team(-5, "Winner B", secondRoundPlayers);
 		Team runnerRoundB = new Team(-6, "Runner-up B", secondRoundPlayers);
-		Match semi1 = new Match(String.format("%s semifinal match 1", m_strName), this, winnerRoundA, runnerRoundA);
+		Match semi1 = new Match(--nMatchId, String.format("%s semifinal match 1", m_strName), this, winnerRoundA, runnerRoundA);
 		m_unscheduled.add(semi1);
 		finalMatch.AddParent(semi1);
-		Match semi2 = new Match(String.format("%s semifinal match 2", m_strName), this, winnerRoundB, runnerRoundB);
+		Match semi2 = new Match(--nMatchId, String.format("%s semifinal match 2", m_strName), this, winnerRoundB, runnerRoundB);
 		m_unscheduled.add(semi2);
 		finalMatch.AddParent(semi2);
 
@@ -62,7 +68,7 @@ public class PoolPlay extends Division {
 		{
 			for (int j = i + 1; j < teams.length / 2; j++)
 			{
-				Match m = new Match(String.format("%s round A match %d", m_strName, i + j), this, teams[i], teams[j]);
+				Match m = new Match(--nMatchId, String.format("%s round A match %d", m_strName, i + j), this, teams[i], teams[j]);
 				m_unscheduled.add(m);
 				semi1.AddParent(m);
 			}
@@ -72,7 +78,7 @@ public class PoolPlay extends Division {
 		{
 			for (int j = i + 1; j < teams.length; j++)
 			{
-				Match m = new Match(String.format("%s round B match %d", m_strName, i + j - (teams.length / 2) * 2), this, teams[i], teams[j]);
+				Match m = new Match(--nMatchId, String.format("%s round B match %d", m_strName, i + j - (teams.length / 2) * 2), this, teams[i], teams[j]);
 				m_unscheduled.add(m);
 				semi2.AddParent(m);
 			}
--- trunk/TournamentScheduler/src/com/RoundRobin.java
+++ trunk/TournamentScheduler/src/com/RoundRobin.java
@@ -30,10 +30,13 @@ public class RoundRobin extends Division {
 		if (m_teams.size() < 3)
 			return;
 
+		int nTeams = m_teams.size();
+		int nMatchId = (nTeams * (nTeams - 1)) / 2;
+
 		Team[] teams = m_teams.toArray(new Team[0]);
 		for (int i = 0; i < teams.length; i++)
 			for (int j = i + 1; j < teams.length; j++)
-				m_unscheduled.add(new Match(String.format("%s match %d", m_strName, i + j), this, teams[i], teams[j]));
+				m_unscheduled.add(new Match(--nMatchId, String.format("%s match %d", m_strName, i + j), this, teams[i], teams[j]));
 	}
 
 }
